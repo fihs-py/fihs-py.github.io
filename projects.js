25073edf38c9abcb1d975a4eb796974804936c05
Fihs <odabasnuri35@gmail.com>
2026-08-30 22:07:22 +0300

    added random stuff

--- projects.js
+++ projects.js
@@ -1,5 +1,15 @@
-const wow = document.getElementById("wow")
+const wow = document.getElementById("wow");
 const inputbox = document.getElementById("input1");
+
+const randomL = document.getElementById("randomL");
+const minL = document.getElementById("min");
+const maxL = document.getElementById("max");
 function getInput(){
     wow.textContent = `You typed "${inputbox.value}" :3`
+}
+function genRandom(){
+    let min = Number(minL.value);
+    let max = Number(maxL.value);
+    
+    randomL.textContent = (Math.floor(Math.random() * (max - min + 1)) + min);
 }
